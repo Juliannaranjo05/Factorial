e30dbd08d38f5a04a9d64dd1648201411d530e34
Yordy Nuñez <yordierik05@gmail.com>
2024-07-23 06:58:44 -0500

    cambio

--- numeros.js
+++ numeros.js
@@ -19,16 +19,17 @@
     console.log(resultado);
     document.getElementById('lista-numero').innerHTML = resultado;
 
-    function factorial(numero) {
-        let resultado = 1;
-        for (let i = numero; i > 1; i--) {
-            resultado = resultado * i;
-        }
-        return resultado;
-    }
     for(let iterar=0;iterar<=5;iterar++){
         let resultado = factorial(iterar);
         mostrar += '<li class="item">El factorial de ' + arregloNumeros[iterar] + ' es ' + resultado + '</li>'
+    }
+
+    function factorial(numero) {
+        let resultado = 1;
+        for (let iteracion = numero; iteracion > 1; iteracion--) {
+            resultado = resultado * iteracion;
+        }
+        return resultado;
     }
 
     document.getElementById('lista-resultado').innerHTML = mostrar;
